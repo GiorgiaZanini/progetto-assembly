e42011813f458381df4b4cbedbf336feb907d27d
GiorgiaZanini <giorgia.zanini.2104@gmail.com>
2024-07-10 11:45:41 +0200

    cambio registro
    sostituisco esi con ebx per poter far la sys, utilizzando poi edx come cotatore, perché ecx serve per la sys

--- asm.s
+++ asm.s
@@ -3,7 +3,7 @@ nome_file:  .asciz "input.txt"
 buffer:     .space 160  # 16 (4 byte * 4 spazi) * 10 (spazie dell'array)
 array:      .long 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
-formato_di_stampa:  .asciz "%d\n"   #TODO 
+formato_di_stampa:  .asciz "%d\n"   //TODO 
                                     //da sistemare per scivere nel file
 
 
@@ -17,10 +17,10 @@ _start
     movl $0, %ecx   # per dire che voglio leggere il file
     int  $0x80
     # il file desciptor viene poi salvato in eax
-    movl %eax, %esi
+    movl %eax, %ebx
 
     movl $0, %ecx   # utilizzo il registro ecx come contatore e lo setto a 0
 
 lettura_e_salvataggio_file:
     movl $3, %eax   # sys_read
-    leal buffer, 
+    leal buffer, %ecx   # prende l'indirizzo di partenza (puntatore) del buffer e lo carica in ecx
