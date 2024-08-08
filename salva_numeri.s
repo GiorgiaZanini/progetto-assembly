a1f508c56acaaefb6389befcae7e8018cea73ac6
GiorgiaZanini <giorgia.zanini.2104@gmail.com>
2024-08-08 11:19:00 +0200

    ricomporre numeri

--- salva_numeri.s
+++ salva_numeri.s
@@ -1,6 +1,8 @@
 .section .data
     ordini_fd: .int -1
-    tmp: .long 1
+    tmp: .long 0
+    array_counter: .long 0
+#    file_counter: .long 0
     array: .long 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0, 000, 00, 000, 0
 
 .section .text
@@ -19,17 +21,17 @@
         int $0x80  
 
 #        test ...   # --> controlla se la sys ha avuto successo
+        cmp $0, %eax
+        jl exit_with_error
 
         # if (eax >= 0 && eax < 10)
-        cmp $0, %eax
-#        cmp $48, %eax   # 0 ascii
-        jl not_in_range     # eax < 0
-        cmp $10, %eax
-#        cmp $57, %eax   # 9 ascii 
-        jge not_in_range    # eax >= 10
-#        jg not_in_range    # eax > 9
-
-        jmp in_0range
+#        cmp $0, %eax
+        cmp $48, %eax   # 0 ascii
+        jl not_in_number_range     # eax < 0
+#        cmp $10, %eax
+        cmp $57, %eax   # 9 ascii 
+#        jge not_in_number_range    # eax >= 10
+        jg not_in_number_range    # eax > 9
 
 
     in_range:   # (numero_salvato * 10) + nuova_cifra
@@ -43,15 +45,27 @@
         jmp read_loop
 
 
-    not_in_range:
-
+    not_in_number_range:
     #    cmp $',', %eax
         cmp $44, %eax   # ',' ascii
+        je salva_in_array
+
+        cmp $12, %eax   # '\n'(line feed) ascii
+        je salva_in_array
+
+        cmp $0, %eax   # 'null' ascii -> fine file
+        je end_read_loop
 
 
-
+    salva_in_array:
+        
 
 
         movl $0, tmp
 
+
+    end_read_loop:
+
+
+    exit_with_error:
         
