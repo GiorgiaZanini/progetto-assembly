517ebda3f344213fb251fcf30f23918675173a05
GiorgiaZanini <giorgia.zanini.2104@gmail.com>
2024-09-08 21:50:11 +0200

    ordinamento_HPF fatto

--- ordinamento_HPF.s
+++ ordinamento_HPF.s
@@ -1,18 +1,15 @@
 .section .data
     puntatore_array_ordini: .long 0     # puntatore all'array dove sono salvati i numeri
     counter_array_ordini: .long 0
-    counter_corrente: .long 2   # terza posizione
+    counter_corrente: .long 3   # quarta posizione
 
-    ok: .ascii "i numeri sono nell'ordine corretto\n\0"
-    inverti: .ascii "i numeri sono da scambiare\n\0"
-    test: .ascii "sono nel controllo per il bubble\n\0"
     a_capo: .ascii "\n\0"
 
 .section .text
-    .global ordinamento_EDF
-    .type ordinamento_EDF, @function
+    .global ordinamento_HPF
+    .type ordinamento_HPF, @function
 
-    ordinamento_EDF:
+    ordinamento_HPF:
         # esi contiene il puntatore a array_ordini
         movl %esi, puntatore_array_ordini
         movl %ecx, counter_array_ordini
@@ -29,7 +26,7 @@
 
         subl $4, %edx # rimuovo il +4 aggiunto per verificare che esista un ordine successivo
 
-    confronto_scadenza:
+    confronto_priority:
         xorl %eax, %eax
         xorl %ebx, %ebx
         movb (%esi,%edx), %al  # sposto il terzo numero dell'ordine puntato in eax
@@ -37,38 +34,28 @@
         addl $4, %edx
         movb (%esi,%edx), %bl    # sposto il terzo numero dell'ordine puntato +1 in ebx
 
-        cmpb %al, %bl   # confronto i due numeri, se il primo è più grande li inverto
-        jl inverti_numeri
-        je confronta_priorita
-
-        pusha
-        leal ok, %eax
-        call stampa_stringa
-        popa
+        cmpb %al, %bl   # confronto i due numeri, se il primo è più piccolo li inverto
+        jg inverti_numeri
+        je confronta_scadenda
 
         jmp controllo_bubble_sort
 
-    confronta_priorita:
+    confronta_scadenda:
         xorl %eax, %eax
         xorl %ebx, %ebx
-        subl $3, %edx
+        subl $5, %edx
         movb (%esi,%edx), %al  # sposto il terzo numero dell'ordine puntato in eax
 
         addl $4, %edx
         movb (%esi,%edx), %bl    # sposto il terzo numero dell'ordine puntato +1 in ebx
 
-        decl %edx
-        cmpb %al, %bl   # confronto i due numeri, se il primo è più piccolo li inverto    
-        jle controllo_bubble_sort
+        incl %edx
+        cmpb %al, %bl   # confronto i due numeri, se il primo è più grande li inverto    
+        jge controllo_bubble_sort
 
     inverti_numeri:
-        pusha
-        leal inverti, %eax
-        call stampa_stringa
-        popa
-
         # pusho il secondo ordine nello stack
-        addl $1, %edx
+#        addl $1, %edx
         movb (%esi, %edx), %bl
         pushl %ebx
 
@@ -123,7 +110,7 @@
         movb %bl, (%esi, %edx)
 
         # ripristino l'indice dell'array alla terza casella del secondo ordine
-        addl $3, %edx
+        addl $4, %edx
 
         pusha
         movl %esi, %eax
@@ -138,13 +125,8 @@
     descrementa_ordini:
         # decrementa di 4 ecx, rimuovendo l'ultimo ordine e resetta edx a 2
         subl $4, %ecx
-        movl $2, %edx
-
-        pusha 
-        leal test, %eax
-        call stampa_stringa
-        popa
-
+        movl $3, %edx
+        
         # continua il ciclo finché ecx non diventa 3
         # sennò salta a fine
         cmpl $3, %ecx
